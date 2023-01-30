c87e06596c09ce82b450ed74a65f92df27367391
Ivan Evdokimov <dr.evdokimov@gmail.com>
2023-01-30 21:35:18 +0000

    Docstring PEP 257

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+"""The module creates character.
+And allows to test battle commands.
+"""
+
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Perform attack action."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
@@ -15,6 +21,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Perform defence action."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +32,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Perform special abilities action."""
     if char_class == 'warrior':
         return (
             f'{char_name} применил специальное'
@@ -37,6 +45,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Perform start training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,6 +70,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Perform select character."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -82,7 +92,9 @@ def choice_char_class() -> str:
     return str(char_class)
 
 
-def main() -> None:
+if __name__ == '__main__':
+    """Main function, runs the mdule."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
